6d1ce12b010082cb0e61131797a7866f052955b7
Cory R Thornsberry <cthornsb@vols.utk.edu>
2015-12-14 16:30:13 -0500

    Added plots to VandleProcessor

--- include/VandleProcessor.hpp
+++ include/VandleProcessor.hpp
@@ -11,13 +11,23 @@ class VandleProcessor : public Processor{
   private:
 	VandleStructure structure;
 	VandleWaveform waveform;  
-  
-	bool HandleEvents();
+
+	Plotter *loc_L_tdiff_2d;
+	Plotter *loc_R_tdiff_2d;
+	Plotter *loc_L_energy_2d;
+	Plotter *loc_R_energy_2d;
+	Plotter *loc_L_phase_2d;
+	Plotter *loc_R_phase_2d;
+	Plotter *loc_1d;
+
+	virtual bool HandleEvents();
 	
   public:
 	VandleProcessor(MapFile *map_);
 
 	~VandleProcessor();
+	
+	virtual void GetHists(std::vector<Plotter*> &plots_);
 };
 
 #endif
--- source/VandleProcessor.cpp
+++ source/VandleProcessor.cpp
@@ -1,10 +1,9 @@
 #include <algorithm>
 #include <cmath>
 
-#include <iomanip>
-
 #include "VandleProcessor.hpp"
 #include "MapFile.hpp"
+#include "Plotter.hpp"
 
 #define C_IN_VAC 29.9792458 // cm/ns
 #define C_IN_BAR 12.65822 // cm/ns
@@ -52,10 +51,26 @@ bool VandleProcessor::HandleEvents(){
 		
 		// Check that the two channels are not separated by too much time.
 		if(absdiff(current_event_L->time, current_event_R->time) > (2 * small_max_tdiff)){ continue; }
+
+		// Calculate the time difference between the current event and the start.
+		double tdiff_L = (current_event_L->time - start->event->time)*8 + (current_event_L->phase - start->event->phase)*4;
+		double tdiff_R = (current_event_R->time - start->event->time)*8 + (current_event_R->phase - start->event->phase)*4;
+		
+		// Get the location of this detector.
+		int location = (*iter_L)->entry->location;
+		
+		// Fill all diagnostic histograms.
+		loc_L_tdiff_2d->Fill(tdiff_L, location);
+		loc_R_tdiff_2d->Fill(tdiff_R, location);
+		loc_L_energy_2d->Fill(current_event_L->hires_energy, location);
+		loc_R_energy_2d->Fill(current_event_R->hires_energy, location);
+		loc_L_phase_2d->Fill(current_event_L->phase, location);
+		loc_R_phase_2d->Fill(current_event_R->phase, location);
+		loc_1d->Fill(location);		
 		
 		// Fill the values into the root tree.
 		structure.Append(current_event_L->hires_energy, current_event_R->hires_energy, current_event_L->time, current_event_R->time,
-		                 current_event_L->phase, current_event_R->phase, (*iter_L)->entry->location);
+		                 current_event_L->phase, current_event_R->phase, location);
 		     
 		// Copy the trace to the output file.
 		if(write_waveform){
@@ -70,6 +85,45 @@ bool VandleProcessor::HandleEvents(){
 VandleProcessor::VandleProcessor(MapFile *map_) : Processor("Vandle", "vandle", map_){
 	root_structure = (Structure*)&structure;
 	root_waveform = (Waveform*)&waveform;
+	
+	int minloc = map_->GetFirstOccurance("vandle");
+	int maxloc = map_->GetLastOccurance("vandle");
+	
+	if(maxloc-minloc > 1){ // More than one detector. Define 2d plots.
+		loc_L_tdiff_2d = new Plotter("generic_h1", "Vandle L Location vs. Tdiff", "COLZ", "Tdiff (ns)", 200, -100, 100, "Location", maxloc-minloc, minloc, maxloc+1);
+		loc_R_tdiff_2d = new Plotter("generic_h2", "Vandle R Location vs. Tdiff", "COLZ", "Tdiff (ns)", 200, -100, 100, "Location", maxloc-minloc, minloc, maxloc+1);
+		loc_L_energy_2d = new Plotter("generic_h3", "Vandle L Location vs. Energy", "COLZ", "Energy (a.u.)", 200, 0, 20000, "Location", maxloc-minloc, minloc, maxloc+1);
+		loc_R_energy_2d = new Plotter("generic_h4", "Vandle R Location vs. Energy", "COLZ", "Energy (a.u.)", 200, 0, 20000, "Location", maxloc-minloc, minloc, maxloc+1);
+		loc_L_phase_2d = new Plotter("generic_h5", "Vandle L Location vs. Phase", "COLZ", "Phase (ns)", 100, 0, 100, "Location", maxloc-minloc, minloc, maxloc+1);
+		loc_R_phase_2d = new Plotter("generic_h6", "Vandle R Location vs. Phase", "COLZ", "Phase (ns)", 100, 0, 100, "Location", maxloc-minloc, minloc, maxloc+1);
+	}
+	else{ // Only one detector. Define 1d plots instead.
+		loc_L_tdiff_2d = new Plotter("generic_h1", "Vandle L Tdiff", "", "Tdiff (ns)", 200, -100, 100);
+		loc_R_tdiff_2d = new Plotter("generic_h2", "Vandle R Tdiff", "", "Tdiff (ns)", 200, -100, 100);
+		loc_L_energy_2d = new Plotter("generic_h3", "Vandle L Energy", "", "Energy (a.u.)", 200, 0, 20000);
+		loc_R_energy_2d = new Plotter("generic_h4", "Vandle R Energy", "", "Energy (a.u.)", 200, 0, 20000);
+		loc_L_phase_2d = new Plotter("generic_h5", "Vandle L Phase", "", "Phase (ns)", 100, 0, 100);
+		loc_R_phase_2d = new Plotter("generic_h6", "Vandle R Phase", "", "Phase (ns)", 100, 0, 100);
+	}
+	loc_1d = new Plotter("generic_h7", "Generic Location", "", "Location", maxloc-minloc, minloc, maxloc+1);
 }
 
-VandleProcessor::~VandleProcessor(){ }
+VandleProcessor::~VandleProcessor(){ 
+	delete loc_L_tdiff_2d;
+	delete loc_R_tdiff_2d;
+	delete loc_L_energy_2d;
+	delete loc_R_energy_2d;
+	delete loc_L_phase_2d;
+	delete loc_R_phase_2d;
+	delete loc_1d;
+}
+
+void VandleProcessor::GetHists(std::vector<Plotter*> &plots_){
+	plots_.push_back(loc_L_tdiff_2d);
+	plots_.push_back(loc_R_tdiff_2d);
+	plots_.push_back(loc_L_energy_2d);
+	plots_.push_back(loc_R_energy_2d);
+	plots_.push_back(loc_L_phase_2d);
+	plots_.push_back(loc_R_phase_2d);
+	plots_.push_back(loc_1d);
+}
