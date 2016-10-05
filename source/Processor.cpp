07b2459df6cf47ceab01a69fc3baad63189b8c35
Cory R Thornsberry <cthornsb@vols.utk.edu>
2016-10-05 11:57:32 -0400

    PreProcess now sets XiaData::energy to tqdc
    Disabled checking of signal-to-noise ratio in
    Processor::PreProcess method.
    
    PreProcess method now sets XiaData::energy to
    ChannelEvent::qdc for processors which use the
    trace to calculate the tqdc.

--- source/Processor.cpp
+++ source/Processor.cpp
@@ -382,10 +382,12 @@ void Processor::PreProcess(){
 			if(current_event->ComputeBaseline() < 0){ continue; }
 		
 			// Check for large SNR.
-			if(current_event->stddev > 3.0){ continue; }
+			//if(current_event->stddev > 3.0){ continue; }
 
-			if(use_integration) // Compute the integral of the pulse within the integration window.
+			if(use_integration){ // Compute the integral of the pulse within the integration window.
 				current_event->IntegratePulse(current_event->max_index - fitting_low, current_event->max_index + fitting_high);
+				current_event->energy = current_event->qdc;
+			}
 		
 			// Set the channel event to valid.
 			current_event->valid_chan = true;
