764ce2ee8b751bb09b7e7c75fe1efc276d11264e
Cory R Thornsberry <cthornsb@vols.utk.edu>
2016-11-02 18:04:22 -0400

    Added ability to read VANDMC mcarlo files to cmbinner

--- tools/source/cmbinner.cpp
+++ tools/source/cmbinner.cpp
@@ -12,11 +12,27 @@
 #include "Structures.h"
 
 class simpleComCalculator : public simpleTool {
+  private:
+	bool mcarlo;
+
   public:
-	simpleComCalculator() : simpleTool(){ }
+	simpleComCalculator() : simpleTool(), mcarlo(false) { }
+	
+	void addOptions();
+	
+	void processArgs();
 	
 	int execute(int argc, char *argv[]);
 };
+
+void simpleComCalculator::addOptions(){
+	addOption(optionExt("mcarlo", no_argument, NULL, 'm', "", "Read from a VANDMC monte carlo file."), userOpts, optstr);
+}
+
+void simpleComCalculator::processArgs(){
+	if(userOpts.at(0).active)
+		mcarlo = true;
+}
 
 int simpleComCalculator::execute(int argc, char *argv[]){
 	if(!setup(argc, argv))
@@ -44,12 +60,19 @@ int simpleComCalculator::execute(int argc, char *argv[]){
 
 	double ctof, energy, theta, angleCOM;
 	
-	TBranch *vandle_b = NULL;
+	TBranch *branch = NULL;
 	VandleStructure *ptr = NULL;
+	std::vector<double> hitTheta;
 
-	intree->SetBranchAddress("vandle", &ptr, &vandle_b);
+	if(!mcarlo){
+		intree->SetBranchAddress("vandle", &ptr, &branch);
+	}
+	else{
+		intree->SetMakeClass(1);
+		intree->SetBranchAddress("hitTheta", &hitTheta, &branch);
+	}
 
-	if(!vandle_b){
+	if(!branch){
 		std::cout << " Error: Failed to load branch \"vandle\" from input TTree.\n";
 		return 6;
 	}
@@ -65,30 +88,43 @@ int simpleComCalculator::execute(int argc, char *argv[]){
 
 	outtree = new TTree("data", "CM Angles");
 	
-	outtree->Branch("tof", &ctof);
-	outtree->Branch("E", &energy);
+	if(!mcarlo){
+		outtree->Branch("tof", &ctof);
+		outtree->Branch("E", &energy);
+	}
 	outtree->Branch("lab", &theta);
 	outtree->Branch("com", &angleCOM);
 	
 	for(unsigned int i = 0; i < intree->GetEntries(); i++){
 		intree->GetEntry(i);
 		
-		for(unsigned int j = 0; j < ptr->ctof.size(); j++){
-			ctof = ptr->ctof.at(j);
-			energy = ptr->energy.at(j);
-			theta = ptr->theta.at(j);
+		if(!mcarlo){
+			for(unsigned int j = 0; j < ptr->ctof.size(); j++){
+				ctof = ptr->ctof.at(j);
+				energy = ptr->energy.at(j);
+				theta = ptr->theta.at(j);
 		
-			rxn.SetLabAngle(theta);
-			angleCOM = rxn.GetEjectile()->comAngle[0];
+				rxn.SetLabAngle(theta);
+				angleCOM = rxn.GetEjectile()->comAngle[0];
 			
-			outtree->Fill();
+				outtree->Fill();
+			}
+		}
+		else{
+			for(unsigned int j = 0; j < hitTheta.size(); j++){
+				theta = hitTheta.at(j);
+				rxn.SetLabAngle(theta);
+				angleCOM = rxn.GetEjectile()->comAngle[0];
+				
+				outtree->Fill();
+			}
 		}
 	}
 	
 	outfile->cd();
 	outtree->Write();
 	
-	std::cout << "\n Done! Wrote " << outtree->GetEntries() << " to '" << output_filename << "'.\n";
+	std::cout << "\n Done! Wrote " << outtree->GetEntries() << " entries to '" << output_filename << "'.\n";
 	
 	return 0;
 }
