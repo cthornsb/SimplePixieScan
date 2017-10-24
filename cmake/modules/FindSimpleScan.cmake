40dc85e3dcb4cfe97548c3cf6858e48338f6f0e6
Cory R. Thornsberry <cthornsb@vols.utk.edu>
2017-10-24 17:15:18 -0400

    Added bin directory to FindSimpleScan module

--- cmake/modules/FindSimpleScan.cmake
+++ cmake/modules/FindSimpleScan.cmake
@@ -6,10 +6,11 @@
 #
 # SimpleScan_FOUND          If the SimpleScan installation is found
 # SimpleScan_INCLUDE_DIR    PATH to the include directory
+# SimpleScan_BIN_DIR        PATH to the bin directory
 # SimpleScan_SCAN_LIB       PATH to the SimpleScan scan library
 # SimpleScan_OPT_LIB        PATH to the option handler library
 #
-# Last updated by C. R. Thornsberry (cthornsb@vols.utk.edu) on Oct. 18th, 2017
+# Last updated by C. R. Thornsberry (cthornsb@vols.utk.edu) on Oct. 24th, 2017
 #
 # - Possible use of this cmake module.
 # Copy the following 4 lines into your main CMakeLists.txt
@@ -31,6 +32,11 @@ find_path(SimpleScan_INCLUDE_DIR
 	PATHS /opt/simpleScan/install
 	PATH_SUFFIXES include)
 
+find_path(SimpleScan_BIN_DIR
+	NAMES simpleScan
+	PATHS /opt/simpleScan/install
+	PATH_SUFFIXES bin)
+
 find_library(SimpleScan_SCAN_LIB
 	NAMES libSimpleScan.so
 	PATHS /opt/simpleScan/install
@@ -43,4 +49,4 @@ find_library(SimpleScan_OPT_LIB
 
 #---Report the status of finding SimpleScan-------------------
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SimpleScan DEFAULT_MSG SimpleScan_INCLUDE_DIR SimpleScan_SCAN_LIB SimpleScan_OPT_LIB)
+find_package_handle_standard_args(SimpleScan DEFAULT_MSG SimpleScan_INCLUDE_DIR SimpleScan_BIN_DIR SimpleScan_SCAN_LIB SimpleScan_OPT_LIB)
